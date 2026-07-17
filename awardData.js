2a6eadbafc4fca6efbe2d0c7c3eb8913ed2df151
SAKSHI RAWAT <95763843+SAKSHI-1226@users.noreply.github.com>
2026-07-17 22:50:22 +0530

    Add files via upload

---
+++ awardData.js
@@ -0,0 +1,50 @@
+const award = {
+
+    title: "SERINGAPATAM TROPHY",
+
+    image: "images/trophy.png",
+
+    details: [
+
+        {
+            label: "Presented to",
+            value: "The British Army in India"
+        },
+
+        {
+            label: "Date of Presentation",
+            value: "IV May MDCCC"
+        },
+
+        {
+            label: "Date of Commemoration",
+            value: "04 Nov 1799"
+        },
+
+        {
+            label: "Occasion",
+            value: "British Victory over Tipu Sultan"
+        },
+
+        {
+            label: "Presented by",
+            value: "Major General George Harris"
+        },
+
+        {
+            label: "Material",
+            value: "Sterling Silver"
+        },
+
+        {
+            label: "Description",
+            value: "Awarded for decisive victory."
+        }
+
+    ],
+
+    summary:
+
+"The medal commemorates the British victory over Tipu Sultan during the Siege of Seringapatam. It represents one of the most significant victories in British India."
+
+}
